b442158bbfe038353982177ee585f931588f8c53
Ralph <ralph.ewig@audacy.space>
2016-10-15 12:08:35 -0700

    new line encoding fix

--- qux-frontend/node-update.sh
+++ qux-frontend/node-update.sh
@@ -4,15 +4,18 @@
 # or executed by hand manually by running this script.
 #
 
+
+echo " "
+echo "====================================================================="
+echo "Updating node code via GitHub pull"
 echo "---------------------------------------------------------------------"
-echo "making temp dir ..."
+echo " "
+
+echo "---------------------------------------------------------------------"
+echo "cloning into temp dir ..."
 echo "---------------------------------------------------------------------"
 mkdir -p /tmp/github-update
 cd /tmp/github-update
-
-
-echo "pulling code from github ..."
-echo "---------------------------------------------------------------------"
 git clone --verbose https://github.com/quindar/quindar-ux.git ./new
 
 echo "---------------------------------------------------------------------"
@@ -20,19 +23,16 @@ echo "cleaning node dir ..."
 echo "---------------------------------------------------------------------"
 rm -rf /node/*
 
-echo "moving new code  ..."
 echo "---------------------------------------------------------------------"
-cp -fvR /tmp/github-update/new/* /node
+echo "installing new code  ..."
+echo "---------------------------------------------------------------------"
+cp -fvR /tmp/github-update/new/* /node/
 cd /node
-
-echo "---------------------------------------------------------------------"
 npm install
+npm stop
+npm start &
 
 echo "---------------------------------------------------------------------"
 echo "cleaning up temp dir ..."
 echo "---------------------------------------------------------------------"
 rm -rf /tmp/github-update
-
-
-# restart the node server to load new code 
-# TBA
