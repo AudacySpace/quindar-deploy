fc0f2a18908f4667ef982dcc72e3c5a0218204f4
Ralph <ralph.ewig@audacy.space>
2016-11-09 18:05:53 -0800

    simplified startup script

--- qsrc-simulator/scripts/vnc_startup.sh
+++ qsrc-simulator/scripts/vnc_startup.sh
@@ -20,17 +20,4 @@ echo -e "\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
 echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/vnc_auto.html?password=..."
 
-for i in "$@"
-do
-case $i in
-    # if option `-t` or `--tail-log` block the execution and tail the VNC log
-    -t|--tail-log)
-    echo -e "\n------------------ /root/.vnc/*$DISPLAY.log ------------------"
-    tail -f /root/.vnc/*$DISPLAY.log
-    ;;
-    *)
-    # unknown option ==> call command
-    exec $i
-    ;;
-esac
-done
+
